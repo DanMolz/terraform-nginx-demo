ce99f204906f78b48b85d9eacab1c8df711ae93b
Daniel Moloney <daniel.moloney@sysdig.com>
2021-07-23 12:55:33 +1000

    .

---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "terraform-demo" {
+  metadata {
+    annotations = {
+      name = "terraform-demo"
+    }
+
+    labels = {
+      mylabel = "terraform-demo"
+    }
+
+    name = "terraform-demo"
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    namespace= "terraform-demo"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
