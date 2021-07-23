4002baf8f44758da79c8446b399fb27ee6d476a3
Daniel Moloney <24286010+SysdigDan@users.noreply.github.com>
2021-07-23 13:27:40 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
-output "nginx service ip address" {
-  value       = aws_db_instance.example.address
-  description = "Connect to the nginx using this ip address"
-}
-
-output "load_balancer_ip" {
+output "nginx_load_balancer_ip" {
   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
+  description = "Connect to the nginx using this ip address"
 }
