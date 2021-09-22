88d715af468554a0d49a534841fda8fc1c58642f
Daniel Moloney <24286010+SysdigDan@users.noreply.github.com>
2021-09-22 10:52:40 +1000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "nginx_load_balancer_ip" {
-  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.ip
+  value = kubernetes_service.terraform-demo-nginx.status.0.load_balancer.0.ingress.0.ip
   description = "Connect to the nginx using this ip address"
 }
